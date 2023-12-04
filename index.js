3128f59f4b718c611ad7c8cfba4b205229b776e2
Shammo06 <mrshammo2018@gmail.com>
2023-12-04 19:15:52 +0600

    add contactDB

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-
 app.use(cors());
 app.use(express.json());
 
@@ -28,10 +27,12 @@ async function run() {
     const biodatasCollection = client.db('BiodatasDB').collection('Biodata');
     const storyCollection = client.db('StoryDB').collection('story');
     const favouriteCollection = client.db('FavouriteDB').collection('favourite');
+    const userCollection = client.db('userDB').collection('user');
+    const contactCollection = client.db('contactDB').collection('data');
     
     app.get('/biodatas', async(req,res) => {
       let query= {}
-      console.log(req.query)
+      
       if(req.query?.biodataType){
         query={biodataType:req.query.biodataType}
       }
@@ -49,7 +50,8 @@ async function run() {
         }
         if(req.query.age){
           query["age"]= {$lte: req.query.age}
-        }        
+        }
+              
       }
       const result = await biodatasCollection.find(query).toArray();
       res.send(result);      
@@ -65,13 +67,88 @@ async function run() {
       const biodata = req.body;
       const result = await biodatasCollection.insertOne(biodata);
       res.send(result);
+    });
+    app.patch('/biodata/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedStatus = req.body;
+      const updateDoc = {
+          $set: {
+              biodataType: updatedStatus.biodataType
+          },
+      };
+      const result = await biodatasCollection.updateOne(filter, updateDoc);
+      res.send(result);
     })
 
+    //favourite
     app.post('/favourite', async (req, res) => {
       const  favourite = req.body;
       const result = await favouriteCollection.insertOne(favourite);
       res.send(result);
     });
+
+    app.get('/favourite', async(req,res) => {
+      let query= {}      
+      if(req.query.userEmail){
+        query={userEmail:req.query.userEmail}
+      }
+      console.log(query)
+      const result = await favouriteCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //SuccessStory
+    app.get('/successStory', async(req,res) => {
+      let query= {}      
+      if(req.query?.email){
+        query={email:req.query.email}
+      }
+      const result = await storyCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //user
+    app.get('/user', async(req,res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/user', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.patch('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updatedStatus = req.body;
+      const updateDoc = {
+          $set: {
+              role: updatedStatus.role
+          },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    //contactCollection
+    app.get('/contactrequest', async(req,res) => {
+      let query= {}      
+      if(req.query?.reqemail){
+        query={reqemail :req.query.reqemail}
+      }
+      const result = await contactCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post('/contactrequest', async (req, res) => {
+      const user = req.body;
+      const result = await contactCollection.insertOne(user);
+      res.send(result);
+    });
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
